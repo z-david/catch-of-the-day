a7c3b90bb32b0a326f7e28ade7e6c24659d17877
z-david <ddwible@gmail.com>
2023-04-30 14:50:47 -0400

    Added a new feature

--- src/components/AddFishForm.js
+++ src/components/AddFishForm.js
@@ -1,20 +1,38 @@
 import React from "react";
 
 class AddFishForm extends React.Component {
-	render() {
+    nameRef = React.createRef();
+    priceRef = React.createRef();
+    statusRef = React.createRef();
+    descRef = React.createRef();
+    imageRef = React.createRef();
+    
+    createFish = (event) => {
+        event.preventDefault();
+        const fish = {
+            name: this.nameRef.current.value,
+            price: this.priceRef.current.value,
+            status: this.statusRef.current.value,
+            desc: this.descRef.current.value,
+            image: this.imageRef.current.value
+        }
+        console.log(fish);
+    }
+	render() { 
 		return (
-			<form class="fish-edit">
-                <input type="text" placeholder="name" name="Name" />
-                <input type="text" placeholder="price"  name="Price" />
-                <select name="Status">
+			<form className="fish-edit" onSubmit={this.createFish}>
+                <input type="text" placeholder="name" name="Name" ref={this.nameRef} />
+                <input type="text" placeholder="price"  name="Price" ref={this.priceRef} />
+                <select name="status" ref={this.statusRef}>
+                    <option value="available">Fresh!</option>
+                    <option value="unavailable">Sold Out</option>
                 </select>
-                <textarea type="text" placeholder="desc" name="Desc" />
-                <input type="text" placeholder="image"  name="Image" />
+                <textarea type="text" placeholder="desc" name="Desc" ref={this.descRef}/>
+                <input type="text" placeholder="image"  name="Image" ref={this.imageRef}/>
                 <button type="submit">+ Add Fish</button> 
             </form>
 		);
 	}
 }
-	
 
 export default AddFishForm
